9a81734714cb680688fcc5ea0e252e42060662a8
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2022-06-27 15:31:43 -0600

    feat: i add testing

---
+++ BackendMegaPetTests/Features/ShelterServiceTest.feature
@@ -0,0 +1,28 @@
+﻿Feature: SheltersServiceTests
+As a Develper 
+I Want to add new Shelter through API
+In Order to make it available for application
+	
+    Background:
+        Given the EndPoint https://https://localhost:7070/api/v1/shelter is available
+		
+    @Shelter-adding
+    Scenario: Add Shelter with Unique Address
+        When a Post Request is sent
+          | address            | image       | Phone     | district | location           |
+          | "NewShelter@gmail" | "image.com" | 978456223 | "Comas"  | "Jr.Matorrales123" |
+        Then A Response with Status 200 is recived
+        And a Shelter Resource is included in Response Body
+          | Id |
+          | 1  |
+    
+    @Shelter-adding
+    Scenario: Add a Shelter with existing Address
+        Given A Shelter is already stored
+        | Id | address            | image       | Phone     | district | location           |
+        | 1  | "NewShelter@gmail" | "image.com" | 978456223 | "Comas"  | "Jr.Matorrales123" |
+        When a Post Request is sent
+          | address            | image       | Phone     | district | location           |
+          | "NewShelter@gmail" | "image.com" | 978456223 | "Comas"  | "Jr.Matorrales123" |
+          Then A Response with Status 400  is recived
+          And An Error Message with value "Shelter Address already exist." is returned
---
+++ BackendMegaPetTests/Steps/SheltersServicesStepDefinition.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BackendMegaPetTests.Steps;
+
+[Binding]
+public sealed class SheltersServicesStepDefinition
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly ScenarioContext _scenarioContext;
+
+    public SheltersServicesStepDefinition(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given("the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [Given("the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+        //TODO: implement arrange (precondition) logic
+        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+        // To use the multiline text or the table argument of the scenario,
+        // additional string/Table parameters can be defined on the step definition
+        // method. 
+
+        _scenarioContext.Pending();
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        //TODO: implement act (action) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        //TODO: implement assert (verification) logic
+
+        _scenarioContext.Pending();
+    }
+
+    [Given(@"the EndPoint https://https://localhost:(.*)/api/v(.*)/shelter is available")]
+    public void GivenTheEndPointHttpsHttpsLocalhostApiVShelterIsAvailable(int p0, int p1)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"a Post Request is sent")]
+    public void WhenAPostRequestIsSent(Table saveShelterResource)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"A Response with Status (.*) is recived")]
+    public void ThenAResponseWithStatusIsReceived(int p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"a Shelter Resource is included in Response Body")]
+    public void ThenAShelterResourceIsIncludedInResponseBody(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"A Shelter is already stored")]
+    public void GivenAShelterIsAlreadyStored(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"An Error Message with value ""(.*)"" is returned")]
+    public void ThenAnErrorMessageWithValueIsReturned(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
